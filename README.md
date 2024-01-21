a631fc2b3c58fc569b0fe20f8d4dc0bc20ab82c0
Leonardo Fernandes <leonardoschoolwork@gmail.com>
2024-01-21 14:56:29 +0530

    Initial Commit

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
-# gdsc-_task_round_frontend_2
-This is a simple Login/SignUp form implemented as task round for Google Developers Student Club VIT Bhopal University
+# React + Vite
 
-deployed link:- https://6589ae2d30feee01da925943--elaborate-hummingbird-a4d566.netlify.app/
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
-Tech Stack:
-1. React: Frontend Framework
-2. Stykled components: Styling and Implementing UI
+Currently, two official plugins are available:
 
+- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
+- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
--- src/App.jsx
+++ src/App.jsx
@@ -1,82 +1,88 @@
+import { FaLinkedin, FaFacebookF, FaGooglePlusG } from "react-icons/fa";
 import { useState } from "react";
-import * as Components from "./Components";
-import { FaFacebook, FaGithub, FaGoogle } from "react-icons/fa";
 import "./App.css";
 
 function App() {
-  const [signIn, toggle] = useState(true);
+  const [isActive, setIsActive] = useState(false);
+
+  const handleClick = () => {
+    setIsActive(!isActive);
+  };
+
   return (
-    <Components.Container>
-      <Components.SignUpContainer signinIn={signIn}>
-        <Components.Form>
-          <Components.Title>Create Account</Components.Title>
-          <Components.Input type="text" placeholder="Name" />
-          <Components.Input type="email" placeholder="Email" />
-          <Components.Input type="password" placeholder="Password" />
-          <Components.ConfirmPasswordInput
-            type="password"
-            placeholder="Confirm Password"
-          />
-          <Components.Button>Sign Up</Components.Button>
-          <Components.SocialButtonsContainer>
-            <Components.SocialButton>
-              <FaFacebook />
-            </Components.SocialButton>
-            <Components.SocialButton>
-              <FaGoogle />
-            </Components.SocialButton>
-            <Components.SocialButton>
-              <FaGithub />
-            </Components.SocialButton>
-          </Components.SocialButtonsContainer>
-        </Components.Form>
-      </Components.SignUpContainer>
-
-      <Components.SignInContainer signinIn={signIn}>
-        <Components.Form>
-          <Components.Title>Sign in</Components.Title>
-          <Components.Input type="email" placeholder="Email" />
-          <Components.Input type="password" placeholder="Password" />
-          <Components.Anchor href="#">Forgot your password?</Components.Anchor>
-          <Components.Button>Sign In</Components.Button>
-          <Components.SocialButtonsContainer>
-            <Components.SocialButton>
-              <FaFacebook />
-            </Components.SocialButton>
-            <Components.SocialButton>
-              <FaGoogle />
-            </Components.SocialButton>
-            <Components.SocialButton>
-              <FaGithub />
-            </Components.SocialButton>
-          </Components.SocialButtonsContainer>
-        </Components.Form>
-      </Components.SignInContainer>
-
-      <Components.OverlayContainer signinIn={signIn}>
-        <Components.Overlay signinIn={signIn}>
-          <Components.LeftOverlayPanel signinIn={signIn}>
-            <Components.Title>Welcome Back!</Components.Title>
-            <Components.Paragraph>
-              To keep connected with us please login to your account
-            </Components.Paragraph>
-            <Components.GhostButton onClick={() => toggle(true)}>
+    <div
+      className={`container ${isActive ? "right-panel-active" : ""}`}
+      id="container"
+    >
+      <div className="form-container sign-up-container">
+        <form action="#">
+          <h1 className="font-bold m-0">Create Account</h1>
+          <div className="social-container">
+            <a href="#" className="social">
+              <FaFacebookF />
+            </a>
+            <a href="#" className="social">
+              <FaGooglePlusG />
+            </a>
+            <a href="#" className="social">
+              <FaLinkedin />
+            </a>
+          </div>
+          <span className="text-[12px]">
+            or use your email for registration
+          </span>
+          <input type="text" placeholder="Name" />
+          <input type="email" placeholder="Email" name="email" />
+          <input type="password" placeholder="Password" />
+          <button>Sign Up</button>
+        </form>
+      </div>
+      <div className="form-container sign-in-container">
+        <form action="#">
+          <h1 className="font-bold m-0">Sign in</h1>
+          <div className="social-container">
+            <a href="#" className="social">
+              <FaFacebookF />
+            </a>
+            <a href="#" className="social">
+              <FaGooglePlusG />
+            </a>
+            <a href="#" className="social">
+              <FaLinkedin />
+            </a>
+          </div>
+          <span className="text-[12px]">or use your account</span>
+          <input type="email" placeholder="Email" name="email" />
+          <input type="password" placeholder="Password" />
+          <a href="#" className="forgot">
+            Forgot your password?
+          </a>
+          <button>Sign In</button>
+        </form>
+      </div>
+      <div className="overlay-container">
+        <div className="overlay">
+          <div className="overlay-panel overlay-left">
+            <h1 className="font-bold m-0">Welcome Back!</h1>
+            <p className=" text-sm font-thin leading-5 tracking-wide my-5 mx-0 mb-7.5">
+              To keep connected with us please login with your personal info
+            </p>
+            <button className="ghost" onClick={handleClick}>
               Sign In
-            </Components.GhostButton>
-          </Components.LeftOverlayPanel>
-
-          <Components.RightOverlayPanel signinIn={signIn}>
-            <Components.Title>No Account</Components.Title>
-            <Components.Paragraph>
-              Sign up and discover a great amount of new opportunities!
-            </Components.Paragraph>
-            <Components.GhostButton onClick={() => toggle(false)}>
-              Sigin Up
-            </Components.GhostButton>
-          </Components.RightOverlayPanel>
-        </Components.Overlay>
-      </Components.OverlayContainer>
-    </Components.Container>
+            </button>
+          </div>
+          <div className="overlay-panel overlay-right">
+            <h1 className="font-bold m-0">Hello, Friend!</h1>
+            <p className=" text-sm font-thin leading-5 tracking-wide my-5 mx-0 mb-7.5">
+              Enter your personal details and start journey with us
+            </p>
+            <button className="ghost" onClick={handleClick}>
+              Sign Up
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
